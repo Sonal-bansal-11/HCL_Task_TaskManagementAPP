3ed20763416e571c812f614799585ecf9fa8e2e8
Sonal-bansal-11 <sonalbansal845@gmail.com>
2026-03-30 13:49:22 +0530

    feat: enable CORS in API and initialize Angular frontend

--- TaskManagement.Api/Program.cs
+++ TaskManagement.Api/Program.cs
@@ -1,10 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManagement.Api.Models;
+using TaskManagement.Api.Repositories;
+using TaskManagement.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // 1. Add services to the container (Dependency Injection)
 builder.Services.AddControllers();
+
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200") // Angular default port
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
 // 2. Register Entity Framework Core In-Memory Database
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -16,18 +29,21 @@ builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 // Register Service
 builder.Services.AddScoped<ITaskService, TaskService>();
 
-// 3. API Documentation Setup
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAngular"); 
 
 // 4. Map Controller Routes
 app.MapControllers();
